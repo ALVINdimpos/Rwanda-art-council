2170cb138a35f66733af3a7ff2937a1ff61e8b33
ALVINdimpos <alvindimpos@gmail.com>
2022-08-11 18:54:51 +0200

    integrated in APi

--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -1,23 +1,48 @@
 import React from "react";
-import NavBar from "../NavBar/Index";
 import Style from "./LoginStyle.module.css";
 import Button from "../Button/Index";
 import { MDBIcon } from "mdbreact";
 import { MDBInput } from "mdb-react-ui-kit";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState } from "react";
+import axios from "axios";
 function Index() {
-  const [Email,SetEmail]=useState('');
-  const [Password,SetPassword]=useState('');
-  const EmailHandler=(e)=>{
+  const [Email, SetEmail] = useState("");
+  const [Password, SetPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
+  const EmailHandler = (e) => {
     SetEmail(e.target.value);
-  }
-  const PasswordHandler=(e)=>{
+  };
+  const PasswordHandler = (e) => {
     SetPassword(e.target.value);
-  }
+  };
+  const loginData = {
+    email: Email,
+    password: Password,
+  };
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+    const res=  await axios.post(
+        "https://rwanda-art-api.herokuapp.com/api/login",
+        loginData
+      );
+      console.log(res)
+      setLoading(false);
+    } catch (error) {
+      SetPassword("");
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
   return (
     <>
-      <NavBar />
       <div className={Style.loginMainWraper}>
         <div className={Style.loginWraper}>
           <div>
@@ -32,25 +57,39 @@ function Index() {
           <div>
             <h1>Sign in</h1>
           </div>
-          <div>
-            <MDBInput
-              id="typeText"
+          <div className={Style.carddetails}>
+            <input
               type="email"
-              placeholder="Enter your email"
+              placeholder="Enter your name"
               onChange={EmailHandler}
+              required
+            />
+            <i className="fa fa-envelope" />
+          </div>
+          <div className={Style.carddetails}>
+            <input
+              type={passwordShown ? "text" : "password"}
+              id="password-input"
+              placeholder="Enter your password"
+              onChange={PasswordHandler}
+              required
+            />
+            <i className="fa fa-lock" />
+            <span>
+              <small className="fa fa-eye-slash passcode" onClick={togglePassword}/>
+            </span>
+          </div>
+          <div>
+            <Button
+              name={loading ? "loading..." : `Login`}
+              onClick={loginHandler}
             />
           </div>
           <div>
-            <MDBInput id="typeText" type="password" placeholder="Password"  onChange={PasswordHandler}/>
-          </div>
-          <div>
-            {" "}
-            <Button name="Login" />
-          </div>
-          <div>
             <p>
-            Don’t have an account?<Link to='/Register' >
-              <span style={{ color: "#c5801a" }}>Sign up</span>
+              Don’t have an account?
+              <Link to="/Register">
+                <span style={{ color: "#c5801a" }}>Sign up</span>
               </Link>
             </p>
           </div>
