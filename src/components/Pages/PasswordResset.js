77bbab5a9661b7c9821a4b60be47c81be1e5d8d2
ALVINdimpos <alvindimpos@gmail.com>
2022-08-13 13:47:44 +0200

    updated

--- src/components/Pages/PasswordResset.js
+++ src/components/Pages/PasswordResset.js
@@ -1,13 +1,59 @@
 import React from "react";
 import Style from "./RestPassward.module.css";
+import { useState } from "react";
+import axios from "axios";
+
 function PasswordResset() {
+  const [email, SetEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const EmailHandler = (e) => {
+    e.preventDefault();
+    SetEmail(e.target.value);
+  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await axios({
+        url:"https://rwanda-art-api.herokuapp.com/api/password/email",
+        data:{email},
+        method:"POST"
+      }
+        
+      );
+      console.log(res);
+      setLoading(false);
+    } catch (error) {
+      SetEmail("");
+      console.log(error);
+      setLoading(false);
+    }
+  };
   return (
-    <div className="container">
+    <div
+      className="container"
+      style={{
+        minHeight: "100vh",
+      }}
+    >
       <div className="row">
         <div className="col-md-4 col-md-offset-4">
           <div className="panel panel-default">
             <div className="panel-body">
-              <div className="text-center">
+              <div
+                className="text-center"
+                style={{
+                  minHeight: "50vh",
+                  paddingTop: "5rem",
+                  backgroundColor: "white",
+                  borderRadius: " 8px",
+                  boxShadow: " 0px 4px 4px 0px #00000040",
+                  padding: "2em",
+                  width: "400px",
+                  marginTop: "2rem",
+                  marginLeft: "20rem",
+                }}
+              >
                 <h3>
                   <i className="fa fa-lock fa-4x" />
                 </h3>
@@ -22,25 +68,24 @@ function PasswordResset() {
                     method="post"
                   >
                     <div className="form-group">
-                      <div className="input-group">
-                        <span className="input-group-addon">
-                          <i className="glyphicon glyphicon-envelope color-blue" />
-                        </span>
+                      <div className={Style.carddetails}>
                         <input
-                          id="email"
-                          name="email"
-                          placeholder="email address"
-                          className="form-control"
                           type="email"
+                          placeholder="Enter your email"
+                          onChange={EmailHandler}
+                          value={email}
+                          required
                         />
+                        <i className="fa fa-envelope" />
                       </div>
                     </div>
                     <div className="form-group">
                       <input
                         name="recover-submit"
                         className="btn btn-lg btn-primary btn-block"
-                        defaultValue="Reset Password"
+                        onClick={submitHandler}
                         type="submit"
+                        value={loading ? "loading..." : `Reset password`}
                       />
                     </div>
                     <input
