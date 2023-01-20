16ad14a0105c839f75929e863e0ab3531dc85079
Perfectizihirwe <perfectizihirwe@gmail.com>
2023-01-20 19:00:42 +0200

    compress images

--- src/components/box/whoWeAreBox/Index.js
+++ src/components/box/whoWeAreBox/Index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Style from "./Style.module.css";
-import whoWeArePhoto from '../../../assets/photos/Back3.jpg'
+import whoWeArePhoto from "../../../assets/photos/Back3.jpg";
 import Button from "../../Button/Index";
 import { useNavigate } from "react-router-dom";
 
 function Index() {
- const navigate =useNavigate();
- const whoHandler=(e)=>{
- e.preventDefault();
- navigate('/Aboutus')
-
-  }
+  const navigate = useNavigate();
+  const whoHandler = (e) => {
+    e.preventDefault();
+    navigate("/Aboutus");
+  };
   return (
     <div>
       <div className={Style.whoWeAreBox}>
@@ -18,16 +17,15 @@ function Index() {
           <h1>WHO WE ARE</h1>
           <h2>We Help To Get Solutions</h2>
           <p>
-          Rwanda Arts Council is None Profit Organization 
-          established by the artists purposly to bringing 
-          together all the artists to advocate for them, and 
-          creating broad lines that help them make profit through their Art.
+            Rwanda Arts Council is None Profit Organization established by the
+            artists purposly to bringing together all the artists to advocate
+            for them, and creating broad lines that help them make profit
+            through their Art.
           </p>
-          <Button name="Read more" onClick={whoHandler}/>
+          <Button name="Read more" onClick={whoHandler} />
         </div>
         <div className={Style.whoWeArePhoto}>
-
-       <img src={whoWeArePhoto} alt="Loading..."/>
+          <img src={whoWeArePhoto} alt="Loading..." />
         </div>
       </div>
     </div>
